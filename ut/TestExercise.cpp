21e918a249ce1b13abaff392b9299b98d5ed2e4a
zonyu <zonyu@nokia.com>
2018-04-16 10:53:11 +0800

    lucky

---
+++ ut/TestExercise.cpp
@@ -0,0 +1,13 @@
+/*
+ * TestExercise.cpp
+ *
+ *  Created on: 2018?4?16?
+ *      Author: zonyu
+ */
+#include "gtest/gtest.h"
+#include "twoSum.hpp"
+
+TEST(twoSum, workingOk)
+{
+    ASSERT_GT(compare(1, 0), 0);
+}
